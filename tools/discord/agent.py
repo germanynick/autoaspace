897111a1580eabca6a69e17db44812cf6d99d246
Duc Pham <ducpmm@gmail.com>
2024-01-31 21:59:04 -0500

    Add langchainhub to Pipfile and import langchain.hub in agent.py

--- tools/discord/agent.py
+++ tools/discord/agent.py
@@ -1,8 +1,9 @@
-from langchain.agents import AgentExecutor, Tool, ZeroShotAgent, create_react_agent
+from langchain.agents import AgentExecutor, Tool, ZeroShotAgent, create_react_agent, create_json_chat_agent
 from langchain.memory import ConversationSummaryMemory, ConversationBufferWindowMemory
 from langchain.tools import DuckDuckGoSearchRun
 from langchain_community.llms import Ollama
 from langchain_core.prompts import PromptTemplate
+from langchain import hub
 
 search = DuckDuckGoSearchRun()
 tools = [
@@ -13,46 +14,41 @@ tools = [
     )
 ]
 
-# template = """
-#     You are a channel support
-#     Your name is {username}, and you are here to help with any questions or problems that arise.
-#     You have access to the following tools:
-
-#             {tools}
-
-#             Use the following format:
-
-#             Question: the input question you must answer
-#             Thought: you should always think about what to do
-#             Action: the action to take, should be one of [{tool_names}]
-#             Action Input: the input to the action
-#             Observation: the result of the action
-#             ... (this Thought/Action/Action Input/Observation can repeat N times)
-#             Thought: I now know the final answer
-#             Final Answer: the final answer to the original input question
-
-#             Begin!
-
-#             Question: {input}
-#             From: {author}
-#             Thought:{agent_scratchpad}
-# """
-
 template = """
-    Act as normal people, write a message as a friend response
-    Tools: {tools}
+    You and {author} are having a conversation.
+    Your name is {username}
+    
+    TOOLS:
+    ------
+
+    You has access to the following tools:
+
+    {tools}
+
+    To use a tool, please use the following format:
+
+    ```
+    Thought: Do I need to use a tool? Yes
     Action: the action to take, should be one of [{tool_names}]
     Action Input: the input to the action
+    Observation: the result of the action
+    ```
 
-    Your name: {username}
-    Message: {input}
-    Author: {author}
+    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
 
-    Thought:{agent_scratchpad}
-    Final Anwser: write a friendly message with the following pattern (@author content)
+    ```
+    Thought: Do I need to use a tool? No
+    Final Answer: [your response here, maximum 20 words]
+    ```
 
-    Chat History:
+    Begin!
+
+    Previous conversation history:
     {chat_history}
+
+    New input: {input}
+    From: {author}
+    {agent_scratchpad}
 """
 
 prompt = PromptTemplate.from_template(template)
@@ -60,7 +56,6 @@ prompt = PromptTemplate.from_template(template)
 llm = Ollama(model="mistral")
 
 memory = ConversationBufferWindowMemory(memory_key="chat_history", k=10, input_key="input")
-
 
 agent = create_react_agent(llm=llm, tools=tools, prompt=prompt)
 
@@ -70,6 +65,6 @@ executor = AgentExecutor(
     verbose=True, 
     memory=memory,
     handle_parsing_errors=True,
-    max_iterations=10
+    max_iterations=10,
 )
 
